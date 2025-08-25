8d5146665191657e32f27fd6e4c261fc9e82213a
Yevgen Berberyan <egnbjork@users.noreply.github.com>
2025-08-25 15:20:07 +0200

    group anagrams optimal solution

--- 4.GroupAnagrams/solution.py
+++ 4.GroupAnagrams/solution.py
@@ -1,11 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        result = {}
-        for i in range(len(strs)):
-            sorted_str = ''.join(sorted(strs[i]))
-            print(sorted_str)
-            if sorted_str in result:
-                result[sorted_str].append(strs[i])
-            else:
-                result[sorted_str] = [strs[i]]
+        result = defaultdict(list)
+        for i in strs:
+            count = [0] * 26
+            for n in i:
+                count[ord(n) - ord("a")] += 1
+            result[tuple(count)].append(i)
         return list(result.values())
