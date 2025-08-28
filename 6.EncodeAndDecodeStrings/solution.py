fbeb31b1a9310f8060bb6e977b06ce6f4888df4f
Yevgen Berberyan <egnbjork@users.noreply.github.com>
2025-08-28 15:17:39 +0200

    encode and decode strings optimal solution

--- 6.EncodeAndDecodeStrings/solution.py
+++ 6.EncodeAndDecodeStrings/solution.py
@@ -3,11 +3,19 @@ class Solution:
     def encode(self, strs: List[str]) -> str:
         res = ""
         for i in strs:
-            res = res + i + "#!#" 
+            res += str(len(i)) + "#" + i
         print(res)
         return res
 
     def decode(self, s: str) -> List[str]:
-        res = s.split("#!#")
-        res.pop()
+        res, i = [], 0
+        while i < len(s):
+            j = i
+            length = ""
+            while s[j] != "#":
+                length += s[j]
+                j = j + 1
+            str = s[j + 1 : (j + 1 + int(length))]
+            i = j + 1 + int(length)
+            res.append(str)
         return res
