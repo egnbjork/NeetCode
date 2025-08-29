ce507556e9e93803e856afa7c2ad8ac95ab5ce09
Yevgen Berberyan <egnbjork@users.noreply.github.com>
2025-08-29 14:52:51 +0200

    products of array except self optimal memory solution

--- 7.ProductsOfArrayExceptSelf/solution.py
+++ 7.ProductsOfArrayExceptSelf/solution.py
@@ -1,28 +1,21 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        res = []
-        prefix = [0] * len(nums)
-        postfix = [0] * len(nums)
+        length = len(nums)
+        res = [1] * length
 
-        for pr_i, pr_n in enumerate(prefix):
-            if pr_i == 0:
-                prefix[pr_i] = nums[pr_i]
-            else:
-                prefix[pr_i] = prefix[pr_i - 1] * nums[pr_i]
+        print(nums)
+        #prefix
+        prefix = 1
+        for i in range(0, length):
+            res[i] = res[i] * prefix
+            prefix = prefix * nums[i]
+        print(res)
+        print(prefix)
 
-        for post_i in range(len(nums) - 1, -1, -1):
-            if post_i == len(nums) - 1:
-                postfix[post_i] = nums[post_i]
-            else:
-                postfix[post_i] = postfix[post_i + 1] * nums[post_i]
+        #postfix
+        postfix = 1
+        for i in range(length - 1, -1, -1):
+            res[i] = res[i] * postfix
+            postfix = postfix * nums[i]
 
-        for i, n in enumerate(nums):
-            if i == 0:
-                res.append(postfix[i + 1])
-            elif i == len(nums) - 1:
-                res.append(prefix[-2])
-            else:
-                num = prefix[i - 1] * postfix[i + 1]
-                res.append(num)
-                
         return res
