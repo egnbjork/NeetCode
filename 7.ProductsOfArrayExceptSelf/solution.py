c59a3a5c14c00e68dc506fafc0bef5c2be904d1c
Yevgen Berberyan <egnbjork@users.noreply.github.com>
2025-08-29 11:33:40 +0200

    products of array except self optimal solution

--- 7.ProductsOfArrayExceptSelf/solution.py
+++ 7.ProductsOfArrayExceptSelf/solution.py
@@ -1,11 +1,28 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         res = []
-        for i1, n in enumerate(nums):
-            prod = 1
-            for i2, p in enumerate(nums):
-                if i2 != i1:
-                    print(f"multiplied {prod} * {p}")
-                    prod = prod * p
-            res.append(prod)
+        prefix = [0] * len(nums)
+        postfix = [0] * len(nums)
+
+        for pr_i, pr_n in enumerate(prefix):
+            if pr_i == 0:
+                prefix[pr_i] = nums[pr_i]
+            else:
+                prefix[pr_i] = prefix[pr_i - 1] * nums[pr_i]
+
+        for post_i in range(len(nums) - 1, -1, -1):
+            if post_i == len(nums) - 1:
+                postfix[post_i] = nums[post_i]
+            else:
+                postfix[post_i] = postfix[post_i + 1] * nums[post_i]
+
+        for i, n in enumerate(nums):
+            if i == 0:
+                res.append(postfix[i + 1])
+            elif i == len(nums) - 1:
+                res.append(prefix[-2])
+            else:
+                num = prefix[i - 1] * postfix[i + 1]
+                res.append(num)
+                
         return res
