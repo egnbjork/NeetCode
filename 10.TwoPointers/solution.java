a053be9930cf3fed6c3346476d365ab656e0b988
Yevgen Berberyan <egnbjork@users.noreply.github.com>
2025-09-03 17:21:32 +0200

    two pointers optimal solution

--- 10.TwoPointers/solution.java
+++ 10.TwoPointers/solution.java
@@ -1,7 +1,30 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        String normalizedString = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
-        String reversedNormalizedString = new StringBuilder(normalizedString).reverse().toString();
-        return normalizedString.equals(reversedNormalizedString);
+        int firstPointer = 0;
+        int secondPointer = s.length() - 1;
+        while(firstPointer <= secondPointer) {
+            if(!isAlphaNumerical(s, firstPointer)) {
+                firstPointer += 1;
+                continue;
+            } 
+            if(!isAlphaNumerical(s, secondPointer)) {
+                secondPointer -= 1;
+                continue;
+            }
+
+            if (Character.toLowerCase(s.charAt(firstPointer)) != Character.toLowerCase(s.charAt(secondPointer))) {
+                return false;
+            }
+            firstPointer += 1;
+            secondPointer -= 1;
+        }
+        return true;
+    }
+
+    private boolean isAlphaNumerical(String s, int pos) {
+        return ((s.charAt(pos) >= 'A' && s.charAt(pos) <= 'Z') ||
+            s.charAt(pos) >= 'a' && s.charAt(pos) <= 'z' ||
+            s.charAt(pos) >= '0' && s.charAt(pos) <= '9');
     }
 }
+
