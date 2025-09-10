e69c0c181a59144877652bddcafedc418f17ec44
Yevgen Berberyan <egnbjork@users.noreply.github.com>
2025-09-10 15:28:36 +0200

    two integer sum II optimal solution

--- 11.TwoIntegerSumII/Solution.java
+++ 11.TwoIntegerSumII/Solution.java
@@ -1,17 +1,16 @@
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
         int firstPointer = 0;
-        int secondPointer = 1;
+        int secondPointer = numbers.length - 1;
         while(firstPointer < numbers.length) {
             int firstNum = numbers[firstPointer];
             int secondNum = numbers[secondPointer];
             if (firstNum + secondNum == target) {
                 return new int[] {firstPointer + 1, secondPointer + 1};
-            }
-            secondPointer++;
-            if(secondPointer == numbers.length) {
+            } else if (firstNum + secondNum < target) {
                 firstPointer++;
-                secondPointer = firstPointer + 1;
+            } else {
+                secondPointer--;
             }
         }
         return new int [0];
